e79e3b6745259d24ffbc692df2206d6fad4d0dd3
ScreamerUWU <64812688+ScreamerUWU@users.noreply.github.com>
2021-05-16 17:52:40 +0100

    Create Roka.lua

---
+++ Games/YBA/REQ/Roka.lua
@@ -0,0 +1,27 @@
+local function UseRoka()
+    local RokaInfo = {
+     "EndDialogue",
+     {NPC = "Rokakaka", Option = "Option1", Dialogue = "Dialogue2"}
+    }
+    
+    if game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Rokakaka") and not game:GetService("Players").LocalPlayer.Character:FindFirstChild("Rokakaka") then
+    
+        local Arrow = game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Rokakaka")
+        
+        Arrow.Parent = game:GetService("Players").LocalPlayer.Character
+        
+        game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(RokaInfo))
+        
+    elseif not game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Rokakaka") and game:GetService("Players").LocalPlayer.Character:FindFirstChild("Rokakaka") then
+       
+        game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(RokaInfo))
+    elseif not game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Rokakaka") and not game:GetService("Players").LocalPlayer.Character:FindFirstChild("Rokakaka") then
+       game.StarterGui:SetCore("SendNotification", {
+          Title = "Rokakaka;",
+          Text = "Wasn't Found!",
+          Duration = 3
+       })
+    end
+end
+
+UseRoka()
