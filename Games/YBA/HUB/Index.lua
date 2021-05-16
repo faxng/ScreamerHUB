edafaa42a784ca087c2605affb08f9638b3b7417
ScreamerUWU <64812688+ScreamerUWU@users.noreply.github.com>
2021-05-16 18:52:57 +0100

    Update Index.lua

--- Games/YBA/HUB/Index.lua
+++ Games/YBA/HUB/Index.lua
@@ -1,19 +1,128 @@
 --[[ UI LIB ]]--
 
-local VapeLIB = ("https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/Vape.txt")
-local VapeMOD = loadstring(syn.request({Url = VapeLIB, Method = "GET", Headers = {}}).Body)()
+local VapeLIB = ("https://raw.githubusercontent.com/ScreamerUWU/ScreamerHUB/main/Games/LIB/Vape.lua")
+local VapeMOD = loadstring(game:HttpGet(VapeLIB))() or loadstring(syn.request({Url = VapeLIB, Method = "GET", Headers = {}}).Body)()
 
---[[ UI WINDOWS ]]--
+--[[ UI WINDOW ]]
 
-local AutoDropWindow = VapeMOD:Window("AUTO DROP")
-local AutoSellWindow = VapeMOD:Window("AUTO SELL")
+local VapeWIN = VapeMOD:Window("Screamer HUB", Color3.fromRGB(0, 255, 0) , Enum.KeyCode.RightControl)
 
-local StandFarmWindow = VapeMOD:Window("STAND FARM")
-local ShinyFarmWindow = VapeMOD:Window("SHINY FARM")
+--[[ UI TABS ]]--
 
-local ItemCounterWindow = VapeMOD:Window("ITEM COUNTER")
+local AutoDropWindow = VapeWIN:Tab("AUTO DROP")
+local AutoSellWindow = VapeWIN:Tab("AUTO SELL")
 
---[[ UI REQUESTS ]]--
+local StandFarmWindow = VapeWIN:Tab("STAND FARM")
+local ShinyFarmWindow = VapeWIN:Tab("SHINY FARM")
 
-local Roka = ("")
-local Arrow = ("")
+local ItemCounterWindow = VapeWIN:Tab("ITEM COUNTER")
+local MiscWindow = VapeWIN:Tab("MISC")
+
+--[[ HTTP REQUESTS ]]--
+
+local Roka = ("https://raw.githubusercontent.com/ScreamerUWU/ScreamerHUB/main/Games/YBA/REQ/Arrow.lua")
+local Arrow = ("https://raw.githubusercontent.com/ScreamerUWU/ScreamerHUB/main/Games/YBA/REQ/Roka.lua")
+local RokaWarn = ("https://raw.githubusercontent.com/ScreamerUWU/ScreamerHUB/main/Games/YBA/REQ/RokaW.lua")
+local WorthTwo = ("https://raw.githubusercontent.com/ScreamerUWU/ScreamerHUB/main/Games/YBA/REQ/WorthTwo.lua")
+local AutoDrop = ("https://raw.githubusercontent.com/ScreamerUWU/ScreamerHUB/main/Games/YBA/REQ/Drop.lua")
+
+--[[ JSON REQUESTS ]]--
+
+local ITEMS = ("https://raw.githubusercontent.com/ScreamerUWU/ScreamerHUB/main/Games/YBA/REQ/Item.JSON")
+
+--[[ SERVICES ]]--
+
+local HttpService = game:GetService("HttpService")
+local PlayerService = game:GetService("Players")
+
+--[[ GLOBAL ]]--
+
+_G.DropAmount = ("ALL")
+_G.DropEnabled = (true)
+_G.CurrentDrop = ("")
+
+_G.SellAmount = ("ALL")
+_G.SellEnabled = (true)
+_G.CurrentSell = ("")
+
+_G.AutoPickupEnabled = (false)
+_G.AutoCount = (false)
+_G.AutoArcade = (false)
+
+_G.StandFarm = (false)
+_G.ShinyFarm = (false)
+_G.StandLogging = (false)
+_G.WebHookLogging = (false)
+
+_G.AFK = (true)
+
+--[[ MAIN FUNCTIONS ]]--
+
+local function GET(URL)
+   
+   local Data = {
+    Url = URL,
+    Method = "GET",
+    Headers = {}
+   }
+ 
+   local Request = game:HttpGet(URL) or (syn.request(Data).Body)
+   
+   warn(Request)
+   
+   return loadstring(Request)()
+end
+
+local function GETBODY(URL)
+   
+   local Data = {
+    Url = URL,
+    Method = "GET",
+    Headers = {}
+   }
+ 
+   local Request = game:HttpGet(URL) or (syn.request(Data).Body)
+   
+   return Request
+end
+
+local function Decode(JSON)
+   return HttpService:JSONDecode(JSON)
+end
+
+local function ChangeProperty(Object, Prop, NewPropValue)
+   Object["Prop"] = (NewPropValue)
+end
+
+--[[ SETUP UI ]]--
+
+AutoDropWindow:Toggle("ENABLED", true, function(Bool)
+    _G.DropEnabled = Bool
+end)
+
+AutoDropWindow:Textbox("AMOUNT", false, function(Input)
+    if not tonumber((Input)) then return end
+    
+    _G.DropAmount = tonumber((Input))
+end)
+
+AutoDropWindow:Button("DROP", function()
+    GET(AutoDrop)
+end)
+
+--[[AutoDropWindow:Button("RESET DROPPING", function()
+    _G.CurrentDrop = ("")
+end)]]
+
+local Dropping = AutoDropWindow:Label("Currently Dropping: N/A")
+
+AutoDropWindow:Label("Items: ")
+
+for Index, Value in pairs(Decode(GETBODY(ITEMS))) do
+   AutoDropWindow:Button(Value, function()
+       
+       _G.CurrentDrop = Value
+       
+       Dropping.Text = ("Currently Dropping: " .. Value)
+   end)
+end
