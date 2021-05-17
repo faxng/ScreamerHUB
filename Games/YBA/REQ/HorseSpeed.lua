9aad81a114a1e58cfe0170e6b92a76fee4384ae3
ScreamerUWU <64812688+ScreamerUWU@users.noreply.github.com>
2021-05-17 17:26:37 +0100

    Update HorseSpeed.lua

--- Games/YBA/REQ/HorseSpeed.lua
+++ Games/YBA/REQ/HorseSpeed.lua
@@ -1,10 +1,13 @@
-local PlayerService = game:GetService("Players")   
+   local PlayerService = game:GetService("Players")   
 
    local function HorseCheck(Seated, SeatObj)
-       
+      warn(tostring(Seated), SeatObj.Parent.Name)
+      
       if Seated then
-         if SeatObj.Parent.Name == (game:GetService("Players").LocalPlayer.Name .. "'s Horse Seat") then
+         if SeatObj.Parent.Name == (game:GetService("Players").LocalPlayer.Name .. "'s Horse") then
             _G.SeatedH = true
+            
+            warn("TRUE!")
          end
       end
 
